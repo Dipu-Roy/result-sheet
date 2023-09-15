f369774cf3253d6c6a6ab61dbbfa6b036ec5707f
Dipu-Roy <dipur319@gmail.com>
2023-09-16 01:01:45 +0600

    all file added

---
+++ GPA.js
@@ -0,0 +1,65 @@
+const mark = 70;
+
+switch (true){
+    // case grade >=95;
+    // console.log("GPA-5");
+    // break;
+
+    case mark >=90:
+        console.log('A+');
+        break;
+
+    case mark >=80:
+        console.log("A");
+        break;
+        
+    case mark >=70:
+        console.log("B+");
+        break;
+        
+    case mark >=60:
+        console.log("B");
+        break;
+        
+    case mark >=50:
+        console.log("c+");
+        break;
+        
+    case mark >=40:
+        console.log("c");
+        break;
+        
+    case mark >=30:
+        console.log("F");
+        break;    
+}
+
+
+// switch (new Date().getDay()) {
+//     case 0;
+//     day = "Sunday";
+//     break;
+
+//      case 1;
+//     day = "Monday";
+//     break;
+    
+//      case 2;
+//     day = "Tuesday";
+//     break;
+
+//     case 3;
+//     day = "Wednesday";
+//     break;
+
+//     case 4;
+//     day = "Thursday";
+//     break;
+
+//     case 5;
+//     day = "Friday";
+//     break;
+    
+//     case 6;
+//     day = "Saturday";
+// }
---
+++ while-break.js
@@ -0,0 +1,8 @@
+let i = 0;
+while(i<100){
+    if(i===80){
+        break;
+    }
+    console.log(i);
+    i+=1;
+}
